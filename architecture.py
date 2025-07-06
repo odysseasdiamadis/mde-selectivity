fixed data scaling and made the model output fmaps as well

architecture.py
@@ -127,6 +127,7 @@ class MV2Block(nn.Module):
                 # pw-linear
                 nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup),
+                nn.ReLU()
             )
         else:
             self.conv = nn.Sequential(
@@ -141,6 +142,7 @@ class MV2Block(nn.Module):
                 # pw-linear
                 nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup),
+                nn.ReLU()
             )
 
     def forward(self, x):
@@ -192,6 +194,8 @@ class MobileViT(nn.Module):
         ph, pw = patch_size
         assert ih % ph == 0 and iw % pw == 0
 
+        self.fmaps = []
+
         L = [1, 1, 1]
 
         self.conv1 = conv_nxn_bn(3, channels[0], stride=2)
@@ -214,24 +218,31 @@ class MobileViT(nn.Module):
 
 
     def forward(self, x):
+        fmaps = []
         y0 = self.conv1(x)
         x = self.mv2[0](y0)
+        fmaps.append(x)
 
         y1 = self.mv2[1](x)
         x = self.mv2[2](y1)
+        fmaps.append(x)
         x = self.mv2[3](x)  # Repeat
+        fmaps.append(x)
 
         y2 = self.mv2[4](x)
         x = self.mvit[0](y2)
+        fmaps.append(x)
 
         y3 = self.mv2[5](x)
+        fmaps.append(x)
         x = self.mvit[1](y3)
 
         x = self.mv2[6](x)
+        fmaps.append(x)
         x = self.mvit[2](x)
         x = self.conv2(x)
 
-        return x, [y0, y1, y2, y3]
+        return x, [y0, y1, y2, y3], fmaps
 
 
 def mobilevit_xxs():
@@ -325,6 +336,6 @@ class build_METER_model(nn.Module):
         self.decoder = decoder(device=device, typ=enc_type)
 
     def forward(self, x):
-        x, enc_layer = self.encoder(x)
+        x, enc_layer, fmaps = self.encoder(x)
         x = self.decoder(x, enc_layer)
-        return x
+        return x, fmaps

data.py
@@ -27,6 +27,7 @@ class NYUDataset(VisionDataset):
                  dtype=torch.float32
                 ):
         super().__init__(root, transforms)
+        self.test = test
         self.dtype = dtype
         self.rgb_transform = t.Compose([
             t.Resize((192,256)),
@@ -38,7 +39,6 @@ class NYUDataset(VisionDataset):
             t.ToImage(),
             t.ToDtype(dtype, scale=(not self.test))
         ])
-        self.test = test
         if test:
             csv_path = os.path.join(root, 'nyu2_test.csv')
         else:
@@ -71,7 +71,7 @@ class NYUDataset(VisionDataset):
         depth = self.depth_transform(depth)
 
         if not self.test:
-            rgb, depth = custom_tensor_augmentation(rgb, depth, dtype=self.dtype)
             depth = depth*1000 # cm
+            rgb, depth = custom_tensor_augmentation(rgb, depth, dtype=self.dtype)
         
         return rgb, depth

evaluate.py
@@ -80,7 +80,6 @@ def compute_dataset_response(
     sum_response = torch.zeros(L, Kmax, D, device=device)
     count_bins = torch.zeros(D, device=device)
 
-    # we will bucketize globally once per batch
     model.eval()
     with torch.no_grad():
         for imgs, depths in loader:
@@ -101,27 +100,7 @@ def compute_dataset_response(
             # accumulate pixel counts
             count_bins += torch.bincount(bidx, minlength=D).to(device)
 
-            # capture each layer’s fmap
-            fmap_list = []
-            hooks = []
-
-            def mk_hook(idx):
-                def hook(_, __, out):
-                    # upsample to (H,W)
-                    up = torch.nn.functional.interpolate(
-                        out, size=(H, W), mode="bilinear", align_corners=False
-                    )
-                    fmap_list.append((idx, up))
-
-                return hook
-
-            for idx, m in enumerate(conv_modules):
-                hooks.append(m.register_forward_hook(mk_hook(idx)))
-
-            _ = model(imgs)
-
-            for h in hooks:
-                h.remove()
+            out, fmap_list = model(imgs)
 
             # now for each layer, scatter‐sum
             for idx, fmap in fmap_list:
@@ -195,12 +174,11 @@ def evaluate(model: nn.Module, dataloader, criterion, device):
         for images, targets in progress_bar:
             images = images.to(device)
             targets = targets.to(device)
-            _ = model(images)  # warm-up step because of image lazy loading
 
             torch.cuda.synchronize()
 
             tic = time.time()
-            outputs = model(images)
+            outputs, fmaps = model(images)
             torch.cuda.synchronize()
             toc = time.time() - tic
             loss = criterion(outputs, targets)

loss.py
@@ -124,7 +124,6 @@ class balanced_loss_function(nn.Module):
 class L_assign(nn.Module):
     def __init__(self,channel_counts, response_compute: "ResponseCompute", lambda_: float, device: torch.device):
         super().__init__()
-        self.channel_counts = channel_counts
         self.lambda_ = lambda_
         self.device = device
         self.response_compute = response_compute
@@ -133,6 +132,7 @@ class L_assign(nn.Module):
                 #  R: torch.Tensor,
                 model,
                 batch,
+                fmaps,
                 device="cuda") -> torch.Tensor:
         """
         R: Tensor of shape [L, K_max, D] containing average responses R_{l,k,d}
@@ -141,10 +141,12 @@ class L_assign(nn.Module):
         
         Returns: scalar loss L_assign
         """
-        R = self.response_compute(model, batch)
+        self.channel_counts = [fmap.shape[1] for fmap in fmaps]
+        R = self.response_compute(model, batch, fmaps)
         L, Kmax, D = R.shape
         
         # Precompute tensors for vectorization
+
         ks = torch.arange(Kmax, device=R.device).unsqueeze(0)  # [1, Kmax]
         channel_counts_t = torch.tensor(self.channel_counts, device=R.device)  # [L]
         
@@ -209,29 +211,29 @@ class ResponseCompute(nn.Module):
         #    mask_count[d]: total # of pixels falling in bin d (shared)
         self.mask_count = torch.zeros(self.D, device=device)
 
-    def forward(self, model: nn.Module, batch: tuple[torch.Tensor,torch.Tensor]):
+    def forward(self, model: nn.Module, batch, fmap_list) -> torch.Tensor:
         imgs, depths = batch
         imgs = imgs.to(self.device)
         depths = depths.to(self.device).squeeze(1)   # → (B, H, W)
         
         # 1) register hooks that just stash the up‐sampled fmap per layer
-        fmap_list: list[torch.Tensor] = []
-        hooks = []
-        def make_hook():
-            def hook(m, _in, out):
-                # out: (B, C, h', w')
-                fmap = F.interpolate(out, size=depths.shape[-2:], 
-                                    mode='bilinear', align_corners=False)
-                fmap_list.append(fmap)  # list of (B, C, H, W)
-            return hook
+        # fmap_list: list[torch.Tensor] = []
+        # hooks = []
+        # def make_hook():
+        #     def hook(m, _in, out):
+        #         # out: (B, C, h', w')
+        #         fmap = F.interpolate(out, size=depths.shape[-2:], 
+        #                             mode='bilinear', align_corners=False)
+        #         fmap_list.append(fmap)  # list of (B, C, H, W)
+        #     return hook
 
-        for conv in self.conv_modules:
-            hooks.append(conv.register_forward_hook(make_hook()))
+        # for conv in self.conv_modules:
+        #     hooks.append(conv.register_forward_hook(make_hook()))
         
-        _ = model(imgs)
+        # _ = model(imgs)
         
-        for h in hooks:
-            h.remove()
+        # for h in hooks:
+        #     h.remove()
 
         # 2) compute global min/max and bin edges
         d_min, d_max = depths.min(), depths.max()
@@ -254,6 +256,9 @@ class ResponseCompute(nn.Module):
 
         for layer_idx, fmap in enumerate(fmap_list):
             # fmap: (B, C, H, W) → (C, B*H*W)
+            fmap = F.interpolate(fmap, size=depths.shape[-2:], 
+                    mode='bilinear', align_corners=False)
+
             C = fmap.shape[1]
             flat_f = fmap.reshape(fmap.shape[0], C, -1).permute(1, 0, 2)
             flat_f = flat_f.reshape(C, -1)  # (C, B*H*W)

train.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 from torchvision.transforms import v2 as t
 
 from architecture import build_METER_model
-from data import NYUDataset, RescaleDepth
+from data import NYUDataset
 from loss import balanced_loss_function
 
 torch.manual_seed(42)

train_selectivity.py
@@ -10,12 +10,13 @@ from tqdm import tqdm
 from torchvision.transforms import v2 as t
 
 from architecture import build_METER_model
-from data import NYUDataset, RescaleDepth
+from data import NYUDataset
 from loss import L_assign, ResponseCompute, balanced_loss_function
 
 torch.manual_seed(42)
 # device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 # torch.set_default_device(device)
+
 
 def load_config(config_path):
     """Load configuration from YAML file."""
@@ -101,7 +102,7 @@ def train_epoch(
 
         optimizer.zero_grad()
 
-        outputs = model(images)
+        outputs, fmaps = model(images)
         loss_base = criterion(outputs, targets)
 
         if log_step:
@@ -115,7 +116,7 @@ def train_epoch(
             )
 
         loss_base = torch.stack(tensors=loss_base, dim=0).sum()
-        loss_assign = l_assign(model, (images, targets))
+        loss_assign = l_assign(model, (images, targets), fmaps)
 
         loss = loss_base + loss_assign
         loss.backward()
@@ -134,7 +135,12 @@ def train_epoch(
         ):
             logging.info(f"Batch {batch_idx}/{num_batches}, Loss: {loss.item():.4f}")
 
-    return epoch_total_loss / num_batches, step_losses, epoch_total_depth_loss / num_batches, epoch_total_selectivity_loss / num_batches
+    return (
+        epoch_total_loss / num_batches,
+        step_losses,
+        epoch_total_depth_loss / num_batches,
+        epoch_total_selectivity_loss / num_batches,
+    )
 
 
 def validate_epoch(model, dataloader, criterion, device):
@@ -150,7 +156,7 @@ def validate_epoch(model, dataloader, criterion, device):
             images = images.to(device)
             targets = targets.to(device)
 
-            outputs = model(images)
+            outputs, fmaps = model(images)
             loss = criterion(outputs, targets)
             loss = torch.stack(loss, dim=0).sum()
 
@@ -181,11 +187,16 @@ def train(config_path, ckpt_path=None) -> None:
 
     model = torch.nn.DataParallel(model, list(range(torch.cuda.device_count())))
     model = model.to(device)
-
     resp_compute = ResponseCompute(model, device=device, config=config, n_of_bins=10)
-    l_assign = L_assign(resp_compute.channel_counts, resp_compute, config['training']['lambda'], device)
+    l_assign = L_assign(
+        resp_compute.channel_counts, resp_compute, config["training"]["lambda"], device
+    )
 
-    optimizer = optim.AdamW(model.parameters(), lr=config["training"]["learning_rate"], weight_decay=0.001)
+    optimizer = optim.AdamW(
+        model.parameters(),
+        lr=config["training"]["learning_rate"],
+        weight_decay=config["training"].get("weight_decay") or 0.01,
+    )
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20)
     criterion = balanced_loss_function(device, dtype=dtype)
     model = model.to(device=device, dtype=dtype)
@@ -214,7 +225,7 @@ def train(config_path, ckpt_path=None) -> None:
         batch_size=config["training"]["batch_size"],
         num_workers=config["data"]["num_workers"],
         pin_memory=pin_memory,
-        shuffle=True
+        shuffle=True,
     )
     val_loader = DataLoader(
         val_ds,